f30450c804f14646b13ef46ce720357e5a2256a6
Hiego <hiegobarreto@gmail.com>
2024-12-07 16:31:19 -0300

    resolvendo erro nas requisições

--- src/main/java/com/example/eng2/domain/dtos/OrganizerResponseDto.java
+++ src/main/java/com/example/eng2/domain/dtos/OrganizerResponseDto.java
@@ -1,19 +1,51 @@
 package com.example.eng2.domain.dtos;
 
-import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class OrganizerResponseDto {
 
     private String name;
     private String email;
     private String phone;
     private String organization;
+
+    public OrganizerResponseDto() {
+    }
+
+    public OrganizerResponseDto(String name, String email, String phone, String organization) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.organization = organization;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
 }
